04c6697744a4d55bdd7de4573dfa8b00fe25e8fa
yonggyu <yonggyu01@naver.com>
2023-12-04 18:11:25 +0900

    추가수정

--- CGV - 영화(주요메인)_새로해야할거_젤최근/js/cgv_theater.js
+++ CGV - 영화(주요메인)_새로해야할거_젤최근/js/cgv_theater.js
@@ -14,11 +14,11 @@ $(document).ready(function () {
 
         }       
     });
-});
+
 
 
 /* fixedNav 스크롤 내릴 시 나타나고 올리면 사라지기 */
-$(document).ready(function(){
+
     var navHeight = $(".fixedNav").height(); 
     //navHeight 의 높이를 구하기
 
@@ -39,7 +39,7 @@ $(document).ready(function(){
             $(".fixedNav").hide();
         }
     });
-});
+
 
 
 
@@ -47,34 +47,34 @@ $(document).ready(function(){
 /* 오른쪽 버튼 클릭시 왼쪽 버튼이 나타나고 
    슬라이드 ul 왼쪽으로 얼만큼 이동
 */
-$(document).ready(function () { 
+
     $(".slide_btn_next").click(function() {
         $(".slide_btn_prev").css({"display": "block"});
         });
-    });
+
 
 
     
 /* theater */
 
 /* city */
-$(document).ready(function () { 
+
     // 마우스 클릭 시 하위메뉴 보이기
     $(".city_tit > li").click(function() {
         $(".area").toggleClass("on");
         });
- });
+
 
  /* grade */ 
- $(document).ready(function () { 
+ 
     // 마우스 클릭 시 팝업메뉴 보이기
     $(".grade").click(function() {
         $(".grade_popup").css({"display": "block"});
      });
- });
+
 
  /* info */ 
-$(document).ready(function(){
+
     // 마우스 클릭 시 팝업메뉴 보이기
     $(".grade").click(function(){
       $(".grade_popup").show();
@@ -83,12 +83,11 @@ $(document).ready(function(){
     $(".grade_close").click(function(){
       $(".grade_popup").hide();
     });
-  });
 
 
 
     // 마우스 오버 시 좌석메뉴 보이기
-$(document).ready(function(){
+
     $(".info_time > li").mouseover(function(){
       // 포지션 가져오기
       const infoTime = $(this).offset();
@@ -99,28 +98,74 @@ $(document).ready(function(){
         left: (infoTime.left - 24) + 'px',
         display: 'block'
       });
-    });
+ 
 
     $(".info_time > li").mouseout(function(){
       // 마우스 아웃 시 숨기기
       $(".minimap_area").css('display', 'none');
     });
   });
+window.onmouseover = function(e){
 
+    let  spacebar = /\n/g
+ 
+    let tagfinder =e.target.tagName 
+    let text='', textresult='';
+ 
+    console.log(textresult)
+    switch (tagfinder) {
+       case 'SPAN':
+           text= e.target.parentElement.parentElement.parentElement.previousElementSibling.innerText
+           console.log(text)
+           textresult  =  text.replace(spacebar, ' ')
+           console.log(textresult)
+           document.querySelector('.data_txt .txt2').innerHTML = textresult;
+           break;
+       case 'EM':
+           text= e.target.parentElement.parentElement.parentElement.previousElementSibling.innerText
+           console.log(text)
+           textresult  =  text.replace(spacebar, ' ')
+           console.log(textresult)
+           document.querySelector('.data_txt .txt2').innerHTML = textresult;
+           break;
+       case 'LI':
+         text = e.target.parentElement.previousElementSibling.innerText
+         console.log(text)
+         textresult  =  text.replace(spacebar, ' ')
+         console.log(textresult)
+         document.querySelector('.data_txt .txt2').innerHTML = textresult;
+           break;
+           default : 
+    }
+
+    console.log(e)
+}
 /* day_list */
-$(document).ready(function () { 
+
     // 클릭시 테두리 on
     $(".day_list").click(function() {
         $(this).toggleClass("on");
         });
- });
 
 
  
+
+
+ 
+
+
+
+// minimap 
+
+ 
+
 /* footer */
-$(document).ready(function () { //HTML 문서가 완전히 로드된 후에 함수 내의 코드를 실행
+//HTML 문서가 완전히 로드된 후에 함수 내의 코드를 실행
     // 마우스 클릭 시 계열사 메뉴 보이기
     $(".familysite").click(function() {
         $(".familysite_wrap dl").toggle();
         });
- });
+
+ 
+
+});
