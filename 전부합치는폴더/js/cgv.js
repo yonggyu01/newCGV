7a3a63ce0b26baff7d02ece333fa94b46cc70516
yonggyu <yonggyu01@naver.com>
2023-12-07 18:02:12 +0900

    디테일 js작업함

--- 전부합치는폴더/js/cgv.js
+++ 전부합치는폴더/js/cgv.js
@@ -2,13 +2,15 @@ function imgchange(url){
    document.querySelector('#special_img').src = url 
 }
 window.onload =function(){
-    let chartnum=0;
+    let chartnum=0,eventnum=0,automode=0,eventtime=0,autotimer;
+    document.querySelector('.familysite_list').style.display ='none'
     window.onclick = function(e){
-       
-        
-        let idchecker = e.target.id || e.target.classList.value;
-        console.log(idchecker)
-        console.log(chartnum)
+    //    e.target.draggable = false
+        let slider = document.querySelector('.slide_list'),//
+        sliderli =document.querySelector('.chart .slide_list li').getBoundingClientRect().width,//
+        eventslide =document.querySelector('.event_list'),//
+        eventli =document.querySelector('.event_list li').getBoundingClientRect().width,//      
+        idchecker = e.target.id || e.target.classList.value;
         switch (idchecker){
             case 'btn_close' :
                 document.querySelector('.adTop').style.display = 'none'
@@ -25,27 +27,165 @@ window.onload =function(){
             case 'btn_soundOff' :
                 document.querySelector('.video1').muted = 1;
                 break;
-            case 'chartpreb' :
+            case 'chartpreb' : //차트 슬라이드 버튼  gap 30px임
+                ++chartnum
                 if(chartnum==0){ e.target.style.display = 'none'}
-                ++chartnum 
-                document.querySelector('.slide_list').style.left =(chartnum*1005)+'px';
+                slider.style.left =chartnum*((sliderli+30)*5)+'px';
+                e.target.nextElementSibling.style.display ='block'
                 break;
             case 'chartnextb' :
-                if(chartnum==-2){ e.target.style.display = 'none'}
-                    --chartnum 
+                --chartnum
+                if(chartnum==-3){ e.target.style.display = 'none'
+                slider.style.left = chartnum*((sliderli+30)*5)+'px'}else{
+                    slider.style.left = chartnum*((sliderli+30)*5)+'px'
+                }
                     e.target.previousElementSibling.style.display ='block'
-                    document.querySelector('.slide_list').style.left = (chartnum*1005)+'px';
-              
                 break;
-
-
+            case 'eventprevb':  //이벤트 버튼  gap 24px임
+            if(eventnum==(eventli +24)*-1){
+                e.target.style.display = 'none'
+                eventnum = eventnum +eventli +24
+                eventslide.style.transform = `translate(${eventnum}px)`
+            }else{
+                eventnum = eventnum +eventli +24
+                eventslide.style.transform = `translate(${eventnum}px)`
+            }
+                e.target.nextElementSibling.style.display ='block'
+                break;
+            case 'eventnextb' :
+                if(eventnum==(((eventli+24)*-1)*2)){
+                    eventnum = eventnum -eventli-24
+                    eventslide.style.transform = `translate(${eventnum}px)`
+                    e.target.style.display = 'none'
+                }else{
+                    eventnum = eventnum -eventli-24
+                    eventslide.style.transform = `translate(${eventnum}px)`
+                }
+                e.target.previousElementSibling.style.display ='block'
+                break;
+                case 'btn_eventControl': //event auto play버튼 처음 브라우저 시작시 플레이 상태임  클릭시 사진 전환필요함
+                automode++
+                e.preventDefault();
+                // 0일때는 스탑임 하자
+                e.target.style.backgroundImage = automode%2 > 0 ?   `url(img/stop.png)` : `url(img/play_d.png)`
+                if(automode%2 > 0){ // 플레이시 할일
+                    autotimer =  setInterval(() => {
+                        //  if와 else로  구성해서  중간에 정지버튼 클릭시 버그 발생가능성 높음
+                         if(eventtime != 0){
+                             if(eventnum==(eventli +24)*-1){
+                                 document.querySelector('#eventnextb').previousElementSibling.style.display = 'none'
+                                 eventnum = eventnum +eventli +24
+                                 eventslide.style.transform = `translate(${eventnum}px)`
+                                 eventtime = 0
+ 
+                             }else{
+                                 eventnum = eventnum +eventli +24
+                                 eventslide.style.transform = `translate(${eventnum}px)`
+                                 document.querySelector('#eventnextb').style.display ='block'
+                             }
+                         }else{
+                             if(eventnum==(((eventli+24)*-1)*2)){
+                                 eventnum = eventnum -eventli-24
+                                 eventslide.style.transform = `translate(${eventnum}px)`
+                                 document.querySelector('#eventnextb').style.display = 'none'
+                                 eventtime = 1 
+                             }else{
+                                 eventnum = eventnum -eventli-24
+                                 eventslide.style.transform = `translate(${eventnum}px)`
+                             }
+                             document.querySelector('#eventnextb').previousElementSibling.style.display ='block'
+                         }
+                     }, 5000) // 버그 가능성이 있으니 추가수정 한번 할것임
+                }else{ // 스탑시
+                    clearInterval(autotimer)
+                    eventtime = 0;
+                    }
+                break;
         }
     }
+    //이벤트 리스트 마우스 호버효과
+    let eventHall = document.querySelectorAll('.specialHall_list li')
+    eventHall.forEach(function(item,idx){
+        item.onmouseenter= function(){
+            item.classList.add('active')
+            imgchange(item.dataset.id)
+            for(i=0;i<eventHall.length; i++){
+                if(idx!=i){
+                    eventHall[i].classList.remove('active')
+                }
+            }
+        }
+        item.onmouseleave = function(){
+            for(i=0;i<eventHall.length; i++){
+                if(idx!=i){
+                    eventHall[i].classList.remove('active')
+                }
+            }
+        }
+    })
 
 
+    document.querySelector('.familysite_wrap h3').onclick = function(){
+        document.querySelector('.familysite_list').style.display =  document.querySelector('.familysite_list').style.display =='none'? 'block' : 'none'
+    }
     let windowYscroll=''
     window.onscroll = function(){
+        let fixnav = document.querySelector('.fixedNav')
+        let footertopbtn = document.querySelector('.fixedBtn')
         windowYscroll=window.pageYOffset;
+        if(document.querySelector('.adTop').style.display!='none' && windowYscroll > 250){
+            fixnav.style.display = 'block'
+            fixnav.style.position = 'fixed'
+            footertopbtn.style.display = 'flex'
+        }else if(document.querySelector('.adTop').style.display=='none' && windowYscroll > 160){
+            fixnav.style.display = 'block'
+            fixnav.style.position = 'fixed'
+            footertopbtn.style.display = 'block'
+        }else{
+            fixnav.style.display = 'none'
+            fixnav.style.position = 'fixed'
+            footertopbtn.style.display = 'none'
+        }
     }
+  // 로컬에 자료저장해야함 .giftcon 태그 속에 무언갈 클릭했을때
+  document.querySelector('.giftcon_wrap').onclick = function(e){
+    // console.log(e)
+    // e.preventDefault();
+    let targetselect,replacetxt=/\n/g;
+    let p = /^..../g;
+    let t =/0[0-9]?.jpg$/g;
 
+
+    switch (e.target.nodeName) {
+        case 'SPAN':
+            targetselect=e.target.parentElement.parentElement
+        break;
+        case 'STRONG':
+            targetselect=e.target.parentElement.parentElement
+        break;
+        case 'A':
+            targetselect=e.target
+        break; 
+        default: 
+        console.log('선택없음')
+        break;
+    }
+    if(targetselect.children[0].localName=='img'){
+        let getsrc =  targetselect.children[0]?.attributes[0].nodeValue
+        let gettext = targetselect.children[1].innerText
+        let resulttxt = gettext.split(replacetxt) 
+        let part = getsrc.replace(p,'');
+        let mid1 = part.replace(t,'');
+
+             console.log(resulttxt)
+        savelocal(getsrc,resulttxt,mid1)
+    }
+    function savelocal(a1,b1,c1){
+        window.localStorage.setItem ('src',a1)
+        window.localStorage.setItem ('text',b1)
+        window.localStorage.setItem ('part',c1)
+        
+    }
+  }
+     
 }
